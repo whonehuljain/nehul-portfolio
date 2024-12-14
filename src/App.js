599224aedd1b1004163d7d3f58f5cd9103bc6cba
Nehul <nehuljain246@gmail.com>
2024-12-15 01:57:08 +0530

    Added vercel analytics package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Portfolio from './components/Portfolio';
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <SpeedInsights />
       <Portfolio />
+      <Analytics />
     </>
   );
 }
