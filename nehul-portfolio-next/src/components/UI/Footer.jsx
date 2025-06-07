c888d674efb0bc3e2cc0be2014eeb18f012bc812
Nehul <nehuljain246@gmail.com>
2025-06-07 21:09:08 +0530

    footer

---
+++ nehul-portfolio-next/src/components/UI/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-gray-300 dark:border-zinc-800 py-6 text-center text-gray-500 dark:text-zinc-500">
+      Made with ❤️ by Nehul
+    </footer>
+  );
+}
