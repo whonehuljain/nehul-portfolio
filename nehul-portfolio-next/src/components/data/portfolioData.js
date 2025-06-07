1c7474d7a6957c816837905cf9ae16a376e14b86
Nehul <nehuljain246@gmail.com>
2025-06-07 21:04:36 +0530

    portfolio data

---
+++ nehul-portfolio-next/src/components/data/portfolioData.js
@@ -0,0 +1,143 @@
+export const experience = [
+  {
+    company: "Grafieks - SDE Intern",
+    link: "https://www.grafieks.com/",
+    logo: "/company-logo/grafieks-logo.png",
+    points: [
+      "developing end-to-end features for Grafieks' BI platform in GoLang, including calculated fields and pivot heatmaps",
+      "leading full-stack development from frontend to deployment, optimizing data processing and performance.",
+    ],
+    period: "Feb 2025 - Present",
+  },
+  {
+    company: "Upsurge Labs - Ml & Backend Development Intern",
+    link: "https://upsurge.io",
+    logo: "/company-logo/upsurge-logo.png",
+    points: [
+      "took cosmo (cosmoflow.ai) from an idea to internal beta, with autonomous web navigation and image element detection powered by open-source models.",
+      "built seamless data pipelines and llm workflows that plugged right into cosmo's interface.",
+      "crafted rest api integrations and database microservices for lemmebuild (lemmebuild.com), a low-code paas platform designed to simplify life (and coding)",
+    ],
+    period: "Aug 2024 - Nov 2024",
+  },
+];
+
+export const projects = [
+  {
+    title: "Robotic Arm RL",
+    description: "Making a two-segment robotic arm stand and balance at 90 degrees in simulation using Reinforcement Learning",
+    image: "/project-logo/rob.jpg",
+    link: "https://github.com/whonehuljain/roboticArm_RL",
+  },
+  {
+    title: "Adv. Library Management System",
+    description: "Built and deployed secure RESTful APIs with advanced analytics for book management, users, borrowing, and fines using PostgreSQL (Prisma) and Render.",
+    image: "/project-logo/adv-lib.webp",
+    link: "https://github.com/whonehuljain/adv-library-ms",
+  },
+  {
+    title: "Babelfish.db",
+    description: "Your universal translator for human-to-database chats",
+    image: "/project-logo/babelfish.jpg",
+    link: "https://babelfish-db.vercel.app/",
+  },
+  {
+    title: "Fnalyze",
+    description: "Predicting Formula 1 Driver Standings with Machine Learning",
+    image: "/project-logo/f1.jpg",
+    link: "https://github.com/whonehuljain/F1nalyze",
+  },
+  {
+    title: "MediMate",
+    description: "AI Health Assistant and Disease Prediction System",
+    image: "/project-logo/medimate.webp",
+    link: "https://github.com/whonehuljain/medimate",
+  },
+  {
+    title: "Text-to-Animation",
+    description: "Leveraged LLMs and 3brown1blue's python library, Manim to create captivating animations from mere prompts. Project is not public for now on github.",
+    image: "/project-logo/tta.png",
+    link: "https://github.com/whonehuljain/text-to-animation",
+  },
+];
+
+export const skills = [
+  {
+    name: "GoLang",
+    icon: "🐹",
+    description: "Concurrency so easy, even a gopher can do it! Fast, clean, and built for scale.",
+    logo: "/skill-logo/gopher-logo.png",
+  },
+  {
+    name: "Kubernetes",
+    icon: "⎈",
+    description: "Why ship one app when you can orchestrate a fleet? Containers, meet your captain!",
+    logo: "/skill-logo/kubernetes-logo.svg",
+  },
+  {
+    name: "Python",
+    icon: "🐍",
+    description: "Every coder's best friend – easy to learn, harder to put down!",
+    logo: "/skill-logo/python-logo.png",
+  },
+  {
+    name: "TensorFlow",
+    icon: "🧠",
+    description: "Sometimes, it's not just about the code, it's about the data!",
+    logo: "/skill-logo/tf-logo.png",
+  },
+  {
+    name: "Django",
+    icon: "dj",
+    description: "Why's there a 'D' if it's just gonna be silent?",
+    logo: "/skill-logo/django-logo.png",
+  },
+  {
+    name: "AWS",
+    icon: "☁️",
+    description: "Badal bahut important hai! ☁️, but dude check those AWS bills!",
+    logo: "/skill-logo/aws-logo.png",
+  },
+  {
+    name: "Flutter",
+    icon: "📱",
+    description: "What? I heard you need an Android dev!?",
+    logo: "/skill-logo/flutter-logo.png",
+  },
+  {
+    name: "Java",
+    icon: "☕",
+    description: "The OG grandparent of programming languages, but still getting the job done! Old is turly gold!",
+    logo: "/skill-logo/java-logo.png",
+  },
+  {
+    name: "JavaScript",
+    icon: "JS",
+    description: "Coz, who needs sleep when you can have endless debugging!?",
+    logo: "/skill-logo/js-logo.svg",
+  },
+  {
+    name: "Figma",
+    icon: "🎨",
+    description: "Coz, Aesthetics Matter!!",
+    logo: "/skill-logo/figma-logo.png",
+  },
+];
+
+export const skillTags = [
+  "Machine Learning",
+  "Data",
+  "Cloud",
+  "DevOps",
+  "Backend",
+  "Mobile",
+];
+
+export const instaPosts = [
+  "https://www.instagram.com/p/C6mVrpRSTgI",
+  "https://www.instagram.com/p/C0rTTzOvLge",
+  "https://www.instagram.com/p/C-QLVRsPABH",
+  "https://www.instagram.com/p/Cygda02vhuw",
+];
+
+export const resumeUrl = "https://drive.google.com/file/d/1tMi40TVuKpwlUmtukdFu-iLoyj1r2-5o/preview";
